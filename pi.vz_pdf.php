149e4947a451e01e6d337392c824180e25a46a62
Eli Van Zoeren <eli@elivz.com>
2011-11-22 10:26:34 -0800

    Basic PDF generation and streaming

--- pi.vz_pdf.php
+++ pi.vz_pdf.php
@@ -12,7 +12,7 @@
 
 $plugin_info = array(
 	'pi_name'		=> 'VZ PDF',
-	'pi_version'	=> '1.0',
+	'pi_version'	=> '0.5.0',
 	'pi_author'		=> 'Eli Van Zoeren',
 	'pi_author_url'	=> 'http://elivz.com',
 	'pi_description'=> 'Generates PDF files using EE templates',
@@ -30,21 +30,61 @@ class Vz_pdf {
 	public function __construct()
 	{
 		$this->EE =& get_instance();
-		$html = $this->EE->TMPL->tagdata;
+    }
+    
+    public function render()
+    {
+		$this->EE =& get_instance();
 		
 		// Get tag parameters
-		$file_name = $this->EE->TMPL->fetch_param('file_name');
+		$html = $this->EE->TMPL->tagdata;
+		$filename = $this->EE->TMPL->fetch_param('filename');
 		$cache_path = $this->EE->TMPL->fetch_param('cache_path');
 		$cache_time = $this->EE->TMPL->fetch_param('refresh', 60);
-		$save_only = $this->EE->TMPL->fetch_param('display', NULL);
+		$save_only = $this->EE->TMPL->fetch_param('display') == 'off';
+		$attachment = $this->EE->TMPL->fetch_param('save_file') == 'yes';
+		
+		if (empty($filename) || empty($html)) return;
+		
+		// Get the PDF data
+		$pdf = $this->_render_pdf($html);
+		
+		// Send the PDF to the browser
+		if (!$save_only)
+		{
+		  $this->_stream_pdf($pdf, $filename);
+		}
+		
+		$this->return_data = '';
+	}
+	
+	private function _render_pdf($html)
+	{
+        // Get parameters
 		$paper_size = $this->EE->TMPL->fetch_param('paper_size', 'letter');
 		$orientation = $this->EE->TMPL->fetch_param('orientation', 'portrait');
-		
-		
-		if (empty($file_name) || empty($html)) return;
-		
-		$this->return_data = "";
+	
+        // Create the PDF
+		require_once("dompdf/dompdf_config.inc.php");
+		$dompdf = new DOMPDF();
+		$dompdf->set_paper($paper_size, $orientation);
+        $dompdf->load_html($html);
+        $dompdf->render();
+        return $dompdf->output();
 	}
+	
+	private function _stream_pdf($pdf, $filename)
+	{
+    	header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Cache-Control: public"); 
+        header("Content-Description: File Transfer"); 
+        header("Content-Type: application/pdf"); 
+        header("Content-Disposition: attachment; filename=$filename");
+        header("Content-Transfer-Encoding: binary");
+        echo $pdf;
+    }
 	
 	// ----------------------------------------------------------------
 	
